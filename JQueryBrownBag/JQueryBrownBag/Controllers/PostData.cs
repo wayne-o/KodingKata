b6287e847c5f02255b8ebffb17540132ad826559
wayne@prjatk.com <wayne@prjatk.com>
2012-07-13 11:16:53 +0100

    Adding JQuery brownbag

---
+++ JQueryBrownBag/JQueryBrownBag/Controllers/PostData.cs
@@ -0,0 +1,9 @@
+namespace JQueryBrownBag.Controllers
+{
+    public class PostData
+    {
+        public string Value1 { get; set; }
+
+        public string Value2 { get; set; }
+    }
+}
---
+++ JQueryBrownBag/JQueryBrownBag/Scripts/application.js
@@ -0,0 +1,62 @@
+﻿$(document).ready(function () {
+    $('.clickmeget').click(function () {
+        $.ajax({
+            url: 'api/values',
+            context: document.body
+        }).done(function (data) {
+            for (var q = 0; q < data.length; q++) {
+                alert(data[q]);
+            }
+        });
+    });
+
+    $('.clickmepost').click(function () {
+        $.ajax({
+            type: 'POST',
+            url: 'api/values',
+            context: document.body,
+            data: {
+                value1: 'testing value 1',
+                value2: 'testing value 2'
+            }
+        }).done(function () {
+            alert('Success');
+        });
+    });
+
+    var closureValue = 'Hello there!!';
+
+    $('.clickmepostandclosure').click(function () {
+        $.ajax({
+            type: 'POST',
+            url: 'api/values',
+            context: document.body,
+            data: {
+                value1: 'testing value 1',
+                value2: 'testing value 2'
+            }
+        }).done(function () {
+            alert(closureValue);
+            //global variable
+            window.nbScope = 'lol';
+        });
+    });
+
+    $('.clickmepostandclosure2').click(function () {
+        $.ajax({
+            type: 'POST',
+            url: 'api/values',
+            context: document.body,
+            data: {
+                value1: 'testing value 1',
+                value2: 'testing value 2'
+            }
+        }).done(closureResponse);
+    });
+
+    var closureResponse = function () {
+        alert(closureValue + ' with no-block-scope!!! ' + window.nbScope);
+    };
+
+});
+
